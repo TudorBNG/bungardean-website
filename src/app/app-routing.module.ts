e34ce68da0225a58965b6e0acfe81d6d6b6aa4a0
Bungărdean Tudor-Ionuț <42916273+IonutBNG@users.noreply.github.com>
2020-10-19 03:25:48 +0300

    Add logo and firebase hosting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'maintenance',
+    component: MaintenanceComponent
+  },
+  { 
+    path: '**', 
+    redirectTo: 'maintenance' 
+  },
+  {
+    path: '404',
+    redirectTo:'maintenance'
+  },
+  {
+    path:'main',
+    component: MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MaintenanceComponent
   ],
   imports: [
     BrowserModule,
